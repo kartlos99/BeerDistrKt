52a8d6e4d5aa0c44bd918841de36f0db0d0f7981
kartlos.diakonidze <kartlos.diakonidze@wandio.com>
2019-12-05 15:11:27 +0400

    data Binding, navigation და რესურსები შევიტანე

---
+++ app/src/main/java/com/example/beerdistrkt/fragment/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.example.beerdistrkt.fragment
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+import com.example.beerdistrkt.R
+import com.example.beerdistrkt.databinding.HomeFragmentBinding
+import com.example.beerdistrkt.viewmodel.HomeViewModel
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: HomeFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.home_fragment, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
