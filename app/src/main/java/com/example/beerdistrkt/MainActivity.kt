ce550f4961c8bd8df4311e0a9b273a27369955ed
kartlos.diakonidze <kartlos.diakonidze@wandio.com>
2019-12-05 16:30:59 +0400

    ჩვენი თულბარი ჩავრთე

--- app/src/main/java/com/example/beerdistrkt/MainActivity.kt
+++ app/src/main/java/com/example/beerdistrkt/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.example.beerdistrkt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.beerdistrkt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var vBinding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        vBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        drawerLayout = vBinding.drawerLayout
+
+        val toolBar = vBinding.toolBar
+        toolBar.title = getString(R.string.location_ge)
+        setSupportActionBar(toolBar)
+
+        val navController = this.findNavController(R.id.mainNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(vBinding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.mainNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
