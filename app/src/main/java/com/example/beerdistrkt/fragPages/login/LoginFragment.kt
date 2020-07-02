f45676ac457766f1df8b225925c2f2df435c4b9a
kartlos.diakonidze <kartlos.diakonidze@wandio.com>
2020-07-03 00:52:51 +0400

    firebase ავტორიზაცია დაემატა

--- app/src/main/java/com/example/beerdistrkt/fragPages/login/LoginFragment.kt
+++ app/src/main/java/com/example/beerdistrkt/fragPages/login/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.beerdistrkt.fragPages.login
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +13,10 @@ import com.example.beerdistrkt.R
 import com.example.beerdistrkt.getViewModel
 import com.example.beerdistrkt.storage.SharedPreferenceDataSource
 import com.example.beerdistrkt.utils.ApiResponseState
+import com.example.beerdistrkt.utils.PrivateKey
 import com.example.beerdistrkt.utils.Session
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import kotlinx.android.synthetic.main.login_fragment.*
 
 class LoginFragment : BaseFragment<LoginViewModel>() {
@@ -23,6 +27,10 @@ class LoginFragment : BaseFragment<LoginViewModel>() {
 
     override val viewModel by lazy {
         getViewModel { LoginViewModel() }
+    }
+
+    private val mAuth by lazy {
+        FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
@@ -39,6 +47,7 @@ class LoginFragment : BaseFragment<LoginViewModel>() {
                 viewLoginUserField.text.toString(),
                 viewLoginPasswordField.text.toString()
             )
+            viewLoginLoginBtn.isEnabled = false
         }
 
         if (Session.get().isUserLogged()) {
@@ -65,18 +74,57 @@ class LoginFragment : BaseFragment<LoginViewModel>() {
         viewModel.loginResponseLiveData.observe(viewLifecycleOwner, Observer {
             when (it) {
                 is ApiResponseState.Success -> {
-                    showToast("login aq")
                     if (viewLoginSaveChk.isChecked) {
                         SharedPreferenceDataSource(requireContext()).saveUserName(it.data.username)
                         SharedPreferenceDataSource(requireContext()).savePassword(
                             viewLoginPasswordField.text.toString()
                         )
                     }
-                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                    loginToFirebase(it.data.username)
                     viewModel.loginResponseLiveData.value = ApiResponseState.Sleep
                 }
             }
         })
+    }
+
+    private fun loginToFirebase(username: String) {
+        val userMail = "$username@apeni.ge"
+        mAuth.signInWithEmailAndPassword(userMail, PrivateKey.FIREBASE_PASS)
+            .addOnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    if (task.exception is FirebaseAuthInvalidUserException) {
+                        registerInFirebase(userMail)
+                        Log.d("auth", "exp_MEssage: " + task.exception?.message)
+                    } else {
+                        showToast(R.string.auth_fail_firebase)
+                        Session.get().clearSession()
+                    }
+                } else {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("auth", "SignIN:success")
+                    onLoginSuccess()
+                }
+            }
+    }
+
+    private fun registerInFirebase(username: String) {
+        mAuth.createUserWithEmailAndPassword(username, PrivateKey.FIREBASE_PASS)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("auth", "createUserWithEmail:success")
+//                    val user = mAuth.currentUser
+                    onLoginSuccess()
+                } else {
+                    Log.d("auth", task.exception?.message ?:"")
+                    showToast(R.string.registration_fail_firebase)
+                    Session.get().clearSession()
+                }
+            }
+    }
+
+    private fun onLoginSuccess() {
+        viewLoginLoginBtn.isEnabled = true
+        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
     }
 
 }
