fd28e63c0be89ad45300252f76ad504b510ed77e
kartlos.diakonidze <kartlos.diakonidze@wandio.com>
2021-05-02 13:26:12 +0400

    დავალიანების ჩვენება ამონაწერის გვერდზე

--- app/src/main/java/com/example/beerdistrkt/fragPages/amonaweri/AmonaweriViewModel.kt
+++ app/src/main/java/com/example/beerdistrkt/fragPages/amonaweri/AmonaweriViewModel.kt
@@ -2,15 +2,22 @@ package com.example.beerdistrkt.fragPages.amonaweri
 
 import androidx.lifecycle.MutableLiveData
 import com.example.beerdistrkt.BaseViewModel
+import com.example.beerdistrkt.models.DataResponse
+import com.example.beerdistrkt.models.DebtResponse
 import com.example.beerdistrkt.models.ObiectWithPrices
+import com.example.beerdistrkt.network.ApeniApiService
+import com.example.beerdistrkt.utils.ApiResponseState
 import kotlinx.coroutines.launch
 
 class AmonaweriViewModel(val clientID: Int) : BaseViewModel() {
 
     val clientLiveData = MutableLiveData<ObiectWithPrices>()
 
+    val getDebtLiveData = MutableLiveData<ApiResponseState<DebtResponse>>()
+
     init {
         getClient()
+        getDebt()
     }
 
     private fun getClient() {
@@ -20,5 +27,20 @@ class AmonaweriViewModel(val clientID: Int) : BaseViewModel() {
                 clientLiveData.value = clientData
             }
         }
+    }
+
+    private fun getDebt() {
+        sendRequest(
+            ApeniApiService.getInstance().getDebt(clientID),
+            successWithData = {
+                getDebtLiveData.value = ApiResponseState.Success(it)
+            },
+            responseFailure = {code, error ->
+                if (code == DataResponse.ErrorCodeDataIsNull)
+                    getDebtLiveData.value = ApiResponseState.Success(
+                        DebtResponse(clientID, "", .0, 0, .0, 0, 0, 0, listOf())
+                    )
+            }
+        )
     }
 }
