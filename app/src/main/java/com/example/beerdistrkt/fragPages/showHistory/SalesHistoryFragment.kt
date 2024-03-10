5b0bd73d1e3822f7ddddf12c97aaaa0217c4eb57
kartlos99 <kartlos99@yahoo.com>
2024-03-10 12:18:30 +0400

    feat bottles:   - add bottle sale history

--- app/src/main/java/com/example/beerdistrkt/fragPages/showHistory/SalesHistoryFragment.kt
+++ app/src/main/java/com/example/beerdistrkt/fragPages/showHistory/SalesHistoryFragment.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.beerdistrkt.BaseFragment
 import com.example.beerdistrkt.R
 import com.example.beerdistrkt.adapters.SimpleListAdapter
 import com.example.beerdistrkt.databinding.FragmentSalesHistoryBinding
+import com.example.beerdistrkt.databinding.ViewBottleSalesHistoryItemBinding
 import com.example.beerdistrkt.databinding.ViewMoneyHistoryItemBinding
 import com.example.beerdistrkt.databinding.ViewSalesHistoryItemBinding
 import com.example.beerdistrkt.fragPages.sawyobi.models.SimpleBeerRowModel
+import com.example.beerdistrkt.fragPages.sawyobi.models.SimpleBottleRowModel
 import com.example.beerdistrkt.getViewModel
 import com.example.beerdistrkt.utils.ApiResponseState
-import com.example.beerdistrkt.utils.visibleIf
 
-class SalesHistoryFragment: BaseFragment<SalesHistoryViewModel>() {
+class SalesHistoryFragment : BaseFragment<SalesHistoryViewModel>() {
 
     private val frBinding by viewBinding(FragmentSalesHistoryBinding::bind)
 
@@ -39,11 +41,17 @@ class SalesHistoryFragment: BaseFragment<SalesHistoryViewModel>() {
         setPageTitle(R.string.history)
         arguments?.let {
             when (it.getString(KEY_HISTORY_OF)) {
-                delivery -> {
+                BARREL_DELIVERY -> {
                     viewModel.getData(it.getInt(KEY_RECORD_ID, 0))
                     setPageTitle(R.string.sale_history_title)
                 }
-                money -> {
+
+                BOTTLE_DELIVERY -> {
+                    viewModel.requestBottleSaleHistory(it.getInt(KEY_RECORD_ID, 0))
+                    setPageTitle(R.string.sale_history_title)
+                }
+
+                MONEY -> {
                     viewModel.getMoneyData(it.getInt(KEY_RECORD_ID, 0))
                     setPageTitle(R.string.money_history_title)
                 }
@@ -54,18 +62,67 @@ class SalesHistoryFragment: BaseFragment<SalesHistoryViewModel>() {
     private fun initViewModel() {
         viewModel.saleHistoryLiveData.observe(viewLifecycleOwner) {
             when (it) {
-                is ApiResponseState.Loading -> frBinding.fragSalesHistoryLoader.visibleIf(it.showLoading)
+                is ApiResponseState.Loading -> frBinding.fragSalesHistoryLoader.isVisible =
+                    it.showLoading
+
                 is ApiResponseState.Success -> showHistory(it.data)
+                else -> showToast(R.string.server_error)
+            }
+        }
+        viewModel.bottleSaleHistoryLiveData.observe(viewLifecycleOwner) {
+            when (it) {
+                is ApiResponseState.Loading -> frBinding.fragSalesHistoryLoader.isVisible =
+                    it.showLoading
+
+                is ApiResponseState.Success -> showBottleHistory(it.data)
                 else -> showToast(R.string.server_error)
             }
         }
         viewModel.moneyHistoryLiveData.observe(viewLifecycleOwner) {
             when (it) {
-                is ApiResponseState.Loading -> frBinding.fragSalesHistoryLoader.visibleIf(it.showLoading)
+                is ApiResponseState.Loading -> frBinding.fragSalesHistoryLoader.isVisible =
+                    it.showLoading
+
                 is ApiResponseState.Success -> showMoneyHistory(it.data)
                 else -> showToast(R.string.server_error)
             }
         }
+    }
+
+    private fun showBottleHistory(historyList: List<BottleSaleHistory>) {
+        if (historyList.isEmpty()) {
+            showToast(getString(R.string.history_not_found))
+            return
+        }
+        frBinding.fragSalesHistoryBaseInfo.text = StringBuilder()
+            .append("ობიექტი: ")
+            .append(historyList.first().client.dasaxeleba)
+            .append("\nდისტრიბუტორი: ")
+            .append(historyList.first().distributor.username)
+
+        frBinding.fragSalesHistoryRc.layoutManager = LinearLayoutManager(context)
+        frBinding.fragSalesHistoryRc.adapter = SimpleListAdapter(
+            historyList,
+            null,
+            R.layout.view_bottle_sales_history_item,
+            onBind = { item, view ->
+                val itemBinding = ViewBottleSalesHistoryItemBinding.bind(view)
+                itemBinding.viewSaleHistoryOperator.text = item.modifyUser.username
+                itemBinding.viewSaleHistoryModifyDate.text = item.modifyDate
+                itemBinding.viewSaleHistoryDate.text =
+                    getString(R.string.date_field, item.saleDate.split(" ")[0])
+                itemBinding.viewSaleHistoryPrice.text =
+                    getString(R.string.price_field, item.unitPrice)
+                itemBinding.viewSaleHistoryComment.isVisible = !item.comment.isNullOrBlank()
+                itemBinding.viewSaleHistoryComment.text = item.comment
+                itemBinding.viewSaleHistoryBottleRow.fillData(
+                    SimpleBottleRowModel(
+                        item.bottle.name,
+                        item.count
+                    )
+                )
+            }
+        )
     }
 
     private fun showMoneyHistory(data: List<MoneyHistory>) {
@@ -85,13 +142,16 @@ class SalesHistoryFragment: BaseFragment<SalesHistoryViewModel>() {
             data,
             null,
             R.layout.view_money_history_item,
-            onBind = {item, view ->
+            onBind = { item, view ->
                 val moneyHistoryBinding = ViewMoneyHistoryItemBinding.bind(view)
                 moneyHistoryBinding.viewMoneyHistoryOperator.text = item.modifyUser.username
                 moneyHistoryBinding.viewMoneyHistoryModifyDate.text = item.modifyDate
-                moneyHistoryBinding.viewMoneyHistoryDate.text = getString(R.string.date_field, item.operationDate.split(" ")[0])
-                moneyHistoryBinding.viewMoneyHistoryAmount.text = getString(R.string.amount_is, item.moneyAmount)
-                moneyHistoryBinding.viewMoneyHistoryComment.visibleIf(!item.comment.isNullOrBlank())
+                moneyHistoryBinding.viewMoneyHistoryDate.text =
+                    getString(R.string.date_field, item.operationDate.split(" ")[0])
+                moneyHistoryBinding.viewMoneyHistoryAmount.text =
+                    getString(R.string.amount_is, item.moneyAmount)
+                moneyHistoryBinding.viewMoneyHistoryComment.isVisible =
+                    !item.comment.isNullOrBlank()
                 moneyHistoryBinding.viewMoneyHistoryComment.text = item.comment
                 moneyHistoryBinding.viewMoneyHistoryPaymentType.setImageResource(item.paymentType.iconRes)
             }
@@ -116,17 +176,25 @@ class SalesHistoryFragment: BaseFragment<SalesHistoryViewModel>() {
             data,
             null,
             R.layout.view_sales_history_item,
-            onBind = {item, view ->
+            onBind = { item, view ->
                 val itemBinding = ViewSalesHistoryItemBinding.bind(view)
                 itemBinding.viewSaleHistoryOperator.text = item.modifyUser.username
                 itemBinding.viewSaleHistoryModifyDate.text = item.modifyDate
-                itemBinding.viewSaleHistoryDate.text = getString(R.string.date_field, item.saleDate.split(" ")[0])
-                itemBinding.viewSaleHistoryPrice.text = getString(R.string.price_field, item.unitPrice)
-                itemBinding.viewSaleHistoryComment.visibleIf(!item.comment.isNullOrBlank())
+                itemBinding.viewSaleHistoryDate.text =
+                    getString(R.string.date_field, item.saleDate.split(" ")[0])
+                itemBinding.viewSaleHistoryPrice.text =
+                    getString(R.string.price_field, item.unitPrice)
+                itemBinding.viewSaleHistoryComment.isVisible = !item.comment.isNullOrBlank()
                 itemBinding.viewSaleHistoryComment.text = item.comment
-                itemBinding.viewSaleHistoryBeerRow.setData(SimpleBeerRowModel(
-                    item.beer.dasaxeleba ?: "", mapOf(item.canTypeID to item.count), null, null, item.beer.displayColor
-                ))
+                itemBinding.viewSaleHistoryBeerRow.setData(
+                    SimpleBeerRowModel(
+                        item.beer.dasaxeleba ?: "",
+                        mapOf(item.canTypeID to item.count),
+                        null,
+                        null,
+                        item.beer.displayColor
+                    )
+                )
             }
         )
         frBinding.fragSalesHistoryRc.adapter = adapter
@@ -136,8 +204,9 @@ class SalesHistoryFragment: BaseFragment<SalesHistoryViewModel>() {
         const val KEY_RECORD_ID = "record_id"
         const val KEY_HISTORY_OF = "KEY_HISTORY_OF"
 
-        const val money = "money"
-        const val delivery = "delivery"
+        const val MONEY = "money"
+        const val BARREL_DELIVERY = "delivery"
+        const val BOTTLE_DELIVERY = "BOTTLE_DELIVERY"
     }
 }
 
