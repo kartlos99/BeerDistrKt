c7184e643f88f5fd9cc99b425b26bfc3be04784e
Kartlos <kartlos99@yahoo.com>
2025-01-12 23:17:12 +0400

    refactor users:   - user row layout changes

--- app/src/main/java/com/example/beerdistrkt/fragPages/user/presentation/list/adapter/UserAdapter.kt
+++ app/src/main/java/com/example/beerdistrkt/fragPages/user/presentation/list/adapter/UserAdapter.kt
@@ -7,23 +7,29 @@ import com.example.beerdistrkt.databinding.UserRowBinding
 import com.example.beerdistrkt.fragPages.user.domain.model.User
 
 class UserAdapter(
-private val dataList: List<User>
-): RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+    private val dataList: List<User>
+) : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
     var onClick: ((userID: String) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(UserRowBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return ViewHolder(
+            UserRowBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun getItemCount(): Int = dataList.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        with(holder.binding) {
-            userRowUserName.text = dataList[position].username
-            userRowDisplayName.text = dataList[position].name
-            root.setOnClickListener {
-                onClick?.invoke(dataList[position].id)
-            }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = with(holder.binding) {
+        val user = dataList[position]
+        userRowUserName.text = user.username
+        userRowDisplayName.text = user.name
+        userType.text = user.type.name
+        root.setOnClickListener {
+            onClick?.invoke(dataList[position].id)
         }
     }
 
