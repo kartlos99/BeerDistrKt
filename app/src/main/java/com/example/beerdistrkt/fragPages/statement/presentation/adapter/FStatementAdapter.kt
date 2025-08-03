0074f57ffb9e183b193a1c0c7cabca17e7575e57
Kartlos <kartlos99@yahoo.com>
2025-08-03 23:59:28 +0400

    extra data in statement: p4   - redesign pay money item in statement

--- app/src/main/java/com/example/beerdistrkt/fragPages/statement/presentation/adapter/FStatementAdapter.kt
+++ app/src/main/java/com/example/beerdistrkt/fragPages/statement/presentation/adapter/FStatementAdapter.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.beerdistrkt.R
 import com.example.beerdistrkt.databinding.StatementListRowBinding
+import com.example.beerdistrkt.databinding.StatementMoneyItemBinding
 import com.example.beerdistrkt.databinding.StatementSaleItemBinding
 import com.example.beerdistrkt.fragPages.statement.domain.model.StatementRecordType
 import com.example.beerdistrkt.fragPages.statement.model.CtxMenuItem
@@ -34,15 +35,18 @@ class FStatementAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
-            0 -> StatementViewHolder(
-                StatementListRowBinding.inflate(LayoutInflater.from(parent.context)),
-                editOldSalePermission,
-                editSalePermission,
-                isGrouped,
-            )
+//            0 -> StatementViewHolder(
+//                StatementListRowBinding.inflate(LayoutInflater.from(parent.context)),
+//                editOldSalePermission,
+//                editSalePermission,
+//                isGrouped,
+//            )
 
+            0 -> StatementMoneyItemViewHolder(
+                StatementMoneyItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            )
             else -> StatementSaleItemViewHolder(
-                StatementSaleItemBinding.inflate(LayoutInflater.from(parent.context)),
+                StatementSaleItemBinding.inflate(LayoutInflater.from(parent.context), parent, false),
             )
         }
     }
@@ -50,7 +54,7 @@ class FStatementAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = getItem(position)
         when (item.recordType) {
-            StatementRecordType.TAKE_MONEY -> (holder as StatementViewHolder).bind(item)
+            StatementRecordType.TAKE_MONEY -> (holder as StatementMoneyItemViewHolder).bind(item)
             else -> (holder as StatementSaleItemViewHolder).bind(item)
         }
     }
@@ -141,6 +145,65 @@ class FStatementAdapter(
         companion object {
             private const val FRICTION_PART_COLOR = "#808080"
             private const val COST_FREE = "უფასო"
+        }
+    }
+
+    class StatementMoneyItemViewHolder(
+        private val binding: StatementMoneyItemBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        private val df = DecimalFormat("#0.00")
+
+        init {
+            binding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        }
+
+        fun bind(item: FinanceStatementUiModel) = with(binding) {
+
+            root.tag = item
+            dateTv.text = item.dateStr
+
+            commentTv.text = item.comment
+
+            val defTextColor = root.context.getAttrColor(R.attr.mainTextColor)
+            val textColor = if (item.comment.isNullOrBlank()) defTextColor else Color.MAGENTA
+
+            val frictionColor = if (item.comment.isNullOrBlank())
+                FRICTION_PART_COLOR.toColorInt()
+            else
+                Color.MAGENTA
+
+            val frSize = root.resources.getDimensionPixelSize(R.dimen.sp12)
+            balanceTv.text = df.format(item.balance).setFrictionSize(frSize, frictionColor)
+            moneyTv.text = getFormattedString(item.pay, df).setFrictionSize(
+                frSize,
+                frictionColor
+            )
+
+            saleTv.setTextColor(textColor)
+            moneyTv.setTextColor(textColor)
+            balanceTv.setTextColor(textColor)
+
+            if (item.comment.isNullOrBlank())
+                commentTv.hide()
+
+            root.setOnClickListener {
+                if (!item.comment.isNullOrBlank())
+                    commentTv.isVisible = !commentTv.isVisible
+            }
+        }
+
+        private fun getFormattedString(value: Double, formatter: DecimalFormat): String =
+            if (value == .0)
+                DASH
+            else
+                formatter.format(value)
+
+        companion object {
+            private const val FRICTION_PART_COLOR = "#808080"
         }
     }
 
